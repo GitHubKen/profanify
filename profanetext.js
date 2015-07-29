e7daffa2300cc4533831ed427b8036e8d0fcfff7
Ken <mobiGITken@users.noreply.github.com>
2015-07-29 19:01:38 -0400

    Update profanetext.js

--- profanetext.js
+++ profanetext.js
@@ -1,14 +1,7 @@
 var profaneWords = ["anus", "arse", "arsehole", "ass", "asshole", "bastard", "bitch", 
 "bloody", "bollocks", "child-fucker", "Christ on a bike", "Christ on a cracker", "cunt", 
 "damn", "fuck", "goddamn", "godsdamn", "hell", "holy shit", "Jesus", "ass-hat", 
-"ass-jabber", "ass-pirate", "assbag", "assbandit, "assbanger", "assbite", "assclown", 
-"asscock", "asscracker", "asses", "assface", "assfuck", "assfucker", "assgoblin", "asshat", 
-"asshead", "asshole", "asshopper", "assjacker", "asslick", "asslicker", "assmonkey", 
-"assmunch", "assmuncher", "assnigger", "asspirate", "assshit", "assshole", "asssucker", 
-"asswad", "asswipe", "axwound", "Jesus Christ", "Jesus H. Christ", "Jesus Harold Christ", 
-"Jesus wept", "Jesus, Mary and Joseph", "Judas Priest", "motherfucker", "shit", "shit ass", 
-"shitass", "son of a bitch", "sob", "son of a motherless goat", "son of a whore", 
-"sweet Jesus"
+"ass-jabber", "ass-pirate", "assbag", "assbandit"
 ];
 var customProfaneWords = [];
 function censor(utteredWords) {
