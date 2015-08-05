0774afbb64723c16ef30d5703c145a03faca504c
Ken <mobiGITken@users.noreply.github.com>
2015-08-05 16:34:39 -0400

    Update profanetext.js

--- profanetext.js
+++ profanetext.js
@@ -1,15 +1,15 @@
 var profaneWords = ["anus", "arse", "arsehole", "ass", "asshole", "bastard", "bitch", 
 "bloody", "bollocks", "child-fucker", "Christ on a bike", "Christ on a cracker", "cunt", 
 "damn", "fuck", "goddamn", "godsdamn", "hell", "holy shit", "Jesus", "ass-hat", 
-"ass-jabber", "ass-pirate", "assbag", "assbandit"
+"ass-jabber", "ass-pirate", "assbag", "assbandit", "motherfucker"
 ];
 var customProfaneWords = [];
 function censor(utteredWords) {
 	for (idx in profaneWords) {
      utteredWords = utteredWords.replace(profaneWords[idx], "expletive");
 	}
-	for (idx in customProfanedWords) {
-     utteredWords = utteredWords.replace(customProfaceWords[idx], "expletive");
+	for (idx in customProfaneWords) {
+     utteredWords = utteredWords.replace(customProfaneWords[idx], "expletive");
 	}
    return utteredWords;
 	}
